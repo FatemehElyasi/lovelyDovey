6cfceb23521e329ca3babadd726f6f6359e66ad3
PC <Fatemeh Elyasi>
2023-07-19 22:23:25 +0430

    add bottom navigation

--- app/src/main/java/ir/fatemelyasi/lovely/MainActivity.kt
+++ app/src/main/java/ir/fatemelyasi/lovely/MainActivity.kt
@@ -2,7 +2,9 @@ package ir.fatemelyasi.lovely
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -11,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import ir.fatemelyasi.lovely.databinding.ActivityMainBinding
 
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +53,53 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, drawerLayout)
         navigationView.setupWithNavController(navController)
-    //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------ActionBarDrawerToggle
+//
+//        val actionBarDrawerToggle = ActionBarDrawerToggle(
+//            this,
+//            binding.drawer,
+//            toolbar,
+//            R.string.navigation_drawer_open,
+//            R.string.navigation_drawer_close
+//        )
+//        binding.drawer.addDrawerListener(actionBarDrawerToggle)
+//        actionBarDrawerToggle.syncState()
+////         click item in drawer
+//        binding.navigationViewMain.setNavigationItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.menu_Anniversary -> {
+//
+//                }
+//                R.id.menu_Setting -> {
+//                    binding.drawer.closeDrawer(GravityCompat.START)
+//                }
+//                R.id.menu_DarkMood -> {
+//
+//                }
+//                R.id.menu_language -> {
+//
+//                }
+//            }
+//            true
+//        }
+
+    //--------------------------------------------------------------------------bottomNavigationView
+        binding.bottomNav.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.menu_story -> {
+
+                }
+                R.id.menu_home -> {
+
+                }
+                R.id.menu_chart -> {
+
+                }
+            }
+            true
+        }
+
+    //-------------------------------------------------------------------------
     }
 
     //--------------------------------------------------------------------------
